16fd669ae3e8acbf9a60b16cf937c4f5f5e3ca6c
Eric <rumperic@gmail.com>
2024-03-01 12:13:19 +0100

    Button fertig

--- lightswitch/script.js
+++ lightswitch/script.js
@@ -1,5 +1,14 @@
-const button = document.querySelector("#lightswitch");
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+let title = false;
 
 button.addEventListener("click", function () {
   body.classList.toggle("dark");
+  button.classList.toggle("aus");
+  title = !title;
+  if (title) {
+    document.title = "Good Night";
+  } else {
+    document.title = "Good Morning";
+  }
 });
