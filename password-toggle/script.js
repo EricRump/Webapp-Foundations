4ed3fa27b284f80254463f5178cc1724c6845422
Eric <rumperic@gmail.com>
2024-03-01 14:45:12 +0100

    aufgabe gelöst, aber farbe ändert sich nicht richtig

---
+++ password-toggle/script.js
@@ -0,0 +1,15 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const div = document.querySelector("div");
+
+button.addEventListener("click", function () {
+  if (input.getAttribute("type") === "text") {
+    input.setAttribute("type", "password");
+    button.textContent = "Show password";
+    div.classList.toggle("red");
+  } else if (input.getAttribute("type") === "password") {
+    input.setAttribute("type", "text");
+    button.textContent = "Hide password";
+    div.classList.toggle("passwort");
+  }
+});
