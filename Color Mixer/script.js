4c150d390b692f59ae7d9aeaf1cd9c240c141ed4
Eric <rumperic@gmail.com>
2024-03-05 15:56:27 +0100

    feat: rgb wird als text angezeigt

--- Color Mixer/script.js
+++ Color Mixer/script.js
@@ -3,10 +3,13 @@ function colorMixer() {
   const green = document.querySelector(".green");
   const blue = document.querySelector(".blue");
   const main = document.querySelector("main");
+  const text = document.querySelector("p");
 
   let redValue = red.value;
   let greenValue = green.value;
   let blueValue = blue.value;
+
+  // Farbe erstellen und dem main hinzufügen
 
   let rgbValue = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
   main.style.backgroundColor = rgbValue;
@@ -14,5 +17,8 @@ function colorMixer() {
   red.addEventListener("input", colorMixer);
   green.addEventListener("input", colorMixer);
   blue.addEventListener("input", colorMixer);
+
+  //RGB als Text anzeigen lassen
+  text.textContent = rgbValue;
 }
 colorMixer();
